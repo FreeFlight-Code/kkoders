3112887d49166466aabe7a4317cad9d0d41ef075
FreeFlight-Code <davidfisc@hotmail.com>
2018-05-21 21:23:52 -0600

    serving files from backend

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+app.listen(3000);
+console.log('Listening on port 3000');
